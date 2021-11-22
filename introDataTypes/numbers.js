273384bd066fe10fdfbafccb1cddae9e8960504b
MarkGraebener <mark.w.graebener@gmail.com>
2021-11-22 15:25:51 -0600

    addind new assignment

---
+++ introDataTypes/numbers.js
@@ -0,0 +1,56 @@
+
+
+console.log('1' + 2)
+// "12"
+
+console.log( '1' + 2)
+//string
+
+console.log(6 % 4)
+//2
+
+// '3 + 4 is ' + 3 + 4
+console.log(3 + 4)
+// how could you make this produce the correct output?
+
+console.log(0 < 0)
+// false
+
+console.log('1' == 1)
+// true wrong its false
+
+console.log( true == 'true')
+//true wrong its false
+
+console.log(5 >= -5)
+// true
+
+console.log( !false || false)
+// true
+
+console.log( true || "42" )
+//true
+
+console.log( !true && !false )
+// false
+
+console.log( 6 % 5 )
+// 1
+
+console.log( 5 < 4 && 1 === 1)
+// false
+
+console.log( typeof 'codeup' === 'string')
+// true
+
+console.log( 'codeup' === 'codeup' && 'codeup' === 'Codeup' )
+// false
+
+console.log( 4 >= 0 && 1 !== '1' )
+//true
+
+console.log( 6 % 3 === 0 )
+//true
+
+console.log( 5 % 2 !== 0 )
+//true
