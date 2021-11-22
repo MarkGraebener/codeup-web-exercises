1b5b34204b45d47a4548b1718672e8be6b1203c5
MarkGraebener <mark.w.graebener@gmail.com>
2021-11-22 14:56:53 -0600

    made dir for intro

---
+++ introDataTypes/strings.js
@@ -0,0 +1,5 @@
+let firstSentance = "Hi, My name is, ";
+let name = "Mark. \n";
+let secondSentance = "I like turtles. \n";
+let lastSentance = "2b or not 2b is a lazy question \n"
+console.log(firstSentance + name + secondSentance + lastSentance)
--- strings.js
+++
@@ -1,5 +0,0 @@
-let firstSentance = "Hi, My name is, ";
-let name = "Mark";
-let secondSentance = " I like turtles.";
-let lastSentance = " 2b or not 2b is a lazy question"
-console.log(firstSentance + name + "." + secondSentance + lastSentance)
