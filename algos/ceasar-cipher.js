c4a202f2509230df138689f8b7157bb86a1feadd
MarkGraebener <mark.w.graebener@gmail.com>
2021-12-15 12:30:13 -0600

    added algos

---
+++ algos/ceasar-cipher.js
@@ -0,0 +1,16 @@
+
+
+//1 crypt function//
+// take in phrase in array//
+// create random num to shift the alphabet from 1 to 26, 0 is not an option
+// create array of each char in string
+// loop through array of chars
+// @ each char use random number to shift current array index to alphabet
+//at each char in string i need to find the index associated with the corresponding alphabet c=2,b=1,f=5 ect....
+// take that index
+// output: a number associated with the shift and letter of shifted value
+const englishAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+//decrypt function//
+//takes in array of shift value  and letters of shifted value//
+// output: an array of a string that is each un shifted number to the original char//
