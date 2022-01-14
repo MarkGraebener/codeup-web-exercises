1a1cb51fd86643078d3c74d1212e723d29afc3ab
MarkGraebener <mark.w.graebener@gmail.com>
2022-01-14 10:29:17 -0600

    algo added

--- algos/ceasar-cipher.js
+++ algos/ceasar-cipher.js
@@ -9,10 +9,25 @@
 //at each char in string i need to find the index associated with the corresponding alphabet c=2,b=1,f=5 ect....
 // take that index
 // output: a number associated with the shift and letter of shifted value
-const englishAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-let makeCyfer = (arr) => {
-
-}
+// const englishAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+// let makeCyfer = (arr) => {
+//
+// }
 //decrypt function//
 //takes in array of shift value  and letters of shifted value//
 // output: an array of a string that is each un shifted number to the original char//
+
+// Make a program that filters through this array: ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"] and returns a new array that contains only those whose name have only 4 letters in them.
+
+let arr = ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"];
+let fourLetters = (arr) => {
+    let arr2 = [];
+    for(let i = 0; i < arr.length; i++){
+        if(i.value.length === 4){
+           arr2 = arr2.push[i];
+           i++;
+        }i++;
+    }
+    return arr2;
+}
+console.log(fourLetters(arr));
