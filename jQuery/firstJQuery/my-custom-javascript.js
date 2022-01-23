d53403f1e0ed07b8587d6ff623e139c4a3e80213
MarkGraebener <mark.w.graebener@gmail.com>
2022-01-22 20:31:31 -0600

    compleated assignment

--- jQuery/firstJQuery/my-custom-javascript.js
+++ jQuery/firstJQuery/my-custom-javascript.js
@@ -1,5 +1,16 @@
 "use strict"
 
+
+
+let alert2 = $("h1");
 $(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+    alert( alert2.text() );
 });
+let alert1 = $("hOneId");
+console.log(alert1);
+$("#hOneId").click(function() {
+    alert("hi" +  alert1.text() );
+});
+
+$(".liClass").css("border", "1px  solid red");
+$(".liClass").css("font-size", "30px");
