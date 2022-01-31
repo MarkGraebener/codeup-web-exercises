39a85a7074d478df7cf2de91b535c77ec2f14125
MarkGraebener <mark.w.graebener@gmail.com>
2022-01-31 14:32:54 -0600

    mapbox

---
+++ jQuery/mapBoxAPI/markers.js
@@ -0,0 +1,11 @@
+// "use strict"
+// import map from "./mapBox.html"
+
+// var perrysMarker = new mapboxgl.Marker()
+//     .setLngLat([ -98.610928, 29.593225])
+//     .addTo(map);
+//
+// var alamoPopup = new mapboxgl.Popup()
+//     .setHTML("<p>Fridays lunch 1/2 off the best porkchop!</p>")
+//
+// perrysMarker.setPopup(alamoPopup)
