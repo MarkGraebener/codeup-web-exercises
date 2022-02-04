8984922c78700f03e632daed630f8f06518fdf48
MarkGraebener <mark.w.graebener@gmail.com>
2022-02-04 11:35:36 -0600

    complete assignment

--- JavaScript/loops/break_and_continue.js
+++ JavaScript/loops/break_and_continue.js
@@ -1,22 +1,39 @@
-"use strict";
+// "use strict";
+//
+// const pickANum = () => {
+//
+//     do {
+//         var number = prompt(" pick an odd number from 1 to 50");
+//        if((!(typeof parseInt(number) === "number" && number % 2 === 1))){
+//            alert("wrong input")
+//        }
+//     }while(!(typeof parseInt(number) === "number" && number % 2 === 1));
+//     if(typeof parseInt(number) === "number" && number % 2 === 1){
+//             for (let i = 1; i <= 50; i += 2) {
+//                 if (i === parseInt(number)) {
+//                     console.log(i + "That's your number!")
+//                     continue;
+//                 }
+//                 console.log(i + "  is not your number!")
+//             }
+//         }
+//
+// }
+// pickANum();
 
-const pickANum = () => {
-
-    do {
-        var number = prompt(" pick an odd number from 1 to 50");
-       if((!(typeof parseInt(number) === "number" && number % 2 === 1))){
-           alert("wrong input")
-       }
-    }while(!(typeof parseInt(number) === "number" && number % 2 === 1));
-    if(typeof parseInt(number) === "number" && number % 2 === 1){
-            for (let i = 1; i <= 50; i += 2) {
-                if (i === parseInt(number)) {
-                    console.log(i + "That's your number!")
-                    continue;
-                }
-                console.log(i + "  is not your number!")
-            }
+function fizzBuzz(n) {
+    // Write your code here
+    for(let i = 0; i <= n; i++) {
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz")
+        }else if (i % 5 === 0){
+            console.log("Buzz")
+        }else if(i % 3 === 0){
+            console.log("Fizz")
+        }else{
+            console.log(i)
         }
-
+    }
 }
-pickANum();
+
+fizzBuzz(30);
