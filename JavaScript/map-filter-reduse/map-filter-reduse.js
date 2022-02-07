4f3864753bfa2aa28e064f9fc2db467bddf307b5
MarkGraebener <mark.w.graebener@gmail.com>
2022-02-07 15:11:24 -0600

    complete

--- JavaScript/map-filter-reduse/map-filter-reduse.js
+++ JavaScript/map-filter-reduse/map-filter-reduse.js
@@ -35,3 +35,53 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
+//1.
+// users.filter(function(x) {
+// let three = [];
+//     if(x.languages.length >= 3){
+//         three.push(x)
+//         console.log(three);
+//     }
+// });
+//2.
+// let three = users.filter(function(n) {
+//     return n.languages.length >= 3
+// });
+// console.log(three);
+
+//3.
+// let emails = users.map(function (n) {
+//     return n.email;
+// });
+// console.log(emails);
+
+
+//4.
+// const aveYears = users.reduce((total, user) => {
+//     console.log(total, user.yearsOfExperience);
+//     return total + user.yearsOfExperience / users.length;
+// }, 0);
+// console.log(aveYears);
+
+// 5.
+// let emails = users.map(function (x) {
+//     return x.email
+// });
+//
+// let longestEmail = emails.reduce(function (longest, current) {
+//     if (current.length > longest.length) {
+//         return current;
+//     } else return longest;
+// }, "");
+// console.log(longestEmail);
+
+//6.
+let nameString = users.reduce((prev, current) => {
+    return prev + current.name + ", "
+}, "Your instructors are: ")
+console.log(nameString);
+
+
+
+
